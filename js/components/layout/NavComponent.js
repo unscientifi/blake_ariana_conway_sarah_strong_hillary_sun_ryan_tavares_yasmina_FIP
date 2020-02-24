8ecbc89fb995c3fdca25586a87e50f0f0291f9b5
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-02-24 00:36:37 -0500

    started building home page

--- js/components/layout/NavComponent.js
+++ js/components/layout/NavComponent.js
@@ -1,10 +1,12 @@
 export default {
     template: `
-    <header class="container" id="mainHeader">
-        <nav class="navbar col-12 navbar-expand-lg navbar-light bg-light">
+    <header id="mainHeader">
+        <nav class="navbar container fixed-top navbar-expand-lg">
             <router-link class="navbar-brand" to="/">project<span>play</span></router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
             </button>
                   
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
