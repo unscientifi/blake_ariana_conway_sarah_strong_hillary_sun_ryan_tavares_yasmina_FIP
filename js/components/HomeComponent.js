a74f660d15356906e1fbf6748171d9929acc1dc6
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-02-26 15:20:30 -0500

    built welcome section

--- js/components/HomeComponent.js
+++ js/components/HomeComponent.js
@@ -1,14 +1,21 @@
 import NavComponent from "./layout/NavComponent.js";
+import CarouselComponent from "./layout/CarouselComponent.js";
+import WelcomeComponent from "./layout/WelcomeComponent.js";
 
 export default {
     template: `
     <section>
         <NavComponent/>
+        <CarouselComponent/>
+        <WelcomeComponent/>
+
     </section>
     `,
 
 
     components: {
         NavComponent: NavComponent,
+        CarouselComponent: CarouselComponent,
+        WelcomeComponent: WelcomeComponent,
     }
 }
--- js/components/layout/NavComponent.js
+++ js/components/layout/NavComponent.js
@@ -2,15 +2,20 @@ export default {
     template: `
     <header id="mainHeader">
         <nav class="navbar container fixed-top navbar-expand-lg">
-            <router-link class="navbar-brand" to="/">project<span>play</span></router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
-            </button>
+            <div class="d-flex flex-grow-1">
+                <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+                <router-link class="navbar-brand" to="/">project<span>play</span></router-link>
+                <div class="w-100 text-right">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                    </button>
+                </div>
+            </div>
                   
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ">
+            <div class="collapse navbar-collapse flex-grow-1 text-right" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto flex-nowrap">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/education">get the facts</router-link>
                     </li>
@@ -23,6 +28,8 @@ export default {
                 </ul>
             </div>
         </nav> 
+
+        
     </header>
     `,
 }
