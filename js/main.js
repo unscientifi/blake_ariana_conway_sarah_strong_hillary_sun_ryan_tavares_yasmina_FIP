edbaad4b4fa42f0775741ab8910b3d94d236793e
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-04-11 17:14:55 -0400

    set up education routes

--- js/main.js
+++ js/main.js
@@ -3,32 +3,34 @@ import AboutComponent from "./components/AboutComponent.js";
 import EducationComponent from "./components/EducationComponent.js";
 import ContactComponent from "./components/ContactComponent.js";
 
-const router = new VueRouter({
-    routes: [
-        { path: "/", name: "home", component: HomeComponent },
-        { path: "/about", name: "about", component: AboutComponent },
-        { path: "/education", name: "education", component: EducationComponent },
-        { path: "/contact", name: "contact", component: ContactComponent }
-
-    ]
-})
-
- var vm = new Vue ({
-     el: '#app',
-
-     data: {},
-
-     mounted() {},
-
-     methods: {},
-
-     router,
-}).$mount("#app");
-
-// check when page has been scrolled so we can add a background colour on the nav
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+(() => {
+    let router = new VueRouter({
+        routes: [
+            { path: "/", name: "home", component: HomeComponent },
+            { path: "/about", name: "about", component: AboutComponent },
+            { path: "/education", name: "education", component: EducationComponent },
+            { path: "/contact", name: "contact", component: ContactComponent }
+        ]
     });
-});
+
+    const vm = new Vue ({
+        el: '#app',
+
+        data: {},
+
+        mounted() {},
+
+        methods: {},
+
+        router: router
+    }).$mount("#app");
+
+    // check when page has been scrolled so we can add a background colour on the nav
+    $(function () {
+        $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+
+})();
